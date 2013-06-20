0c46e2bd2ad8e3af6f5e7b056c479d85cd30de7b
Aazmp <aazmp@yandex.ru>
2013-06-20 23:14:43 +0400

    Create stdint1.h

---
+++ stdint1.h
@@ -0,0 +1,7 @@
+typedef short int16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef unsigned short uint16_t;
